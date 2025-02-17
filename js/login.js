97713b551e53fdf7b2c6dbda3b6a369744104fb9
Iyvena <szalay.viktoria.1049@dszcbaross.edu.hu>
2025-02-17 10:02:57 +0100

    login

--- js/login.js
+++ js/login.js
@@ -6,7 +6,7 @@ async function login() {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    const res = await fetch('https://nodejs313.dszcbaross.edu.hu/login', {
+    const res = await fetch('https://nodejs313.dszcbaross.edu.hu/api/auth/login', {
         method: 'POST',
         headers: {
             'content-type': 'application/json'
