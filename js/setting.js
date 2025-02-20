b135ba95e980550ae7be5da59bee2437db3be182
Iyvena <szalay.viktoria.1049@dszcbaross.edu.hu>
2025-02-20 09:30:09 +0100

    profile changes

---
+++ js/setting.js
@@ -0,0 +1,15 @@
+const btnName = document.getElementById('editName');
+const btnPsw = document.getElementById('editPsw');
+const btnAddress = document.getElementById('editAddress');
+
+btnName.addEventListener('click', () => {
+    window.location.href = '../profileName.html';
+});
+
+btnPsw.addEventListener('click', () => {
+    window.location.href = '../profilePsw.html';
+});
+
+btnAddress.addEventListener('click', () => {
+    window.location.href = '../address.html';
+});
