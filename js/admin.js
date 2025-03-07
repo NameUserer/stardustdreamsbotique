60d61dc1b4bc09bbb98aab774e09dbdb8ca081c9
Iyvena <szalay.viktoria.1049@dszcbaross.edu.hu>
2025-03-07 10:59:44 +0100

    delete

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,9 @@
 const addbutton = document.getElementsByClassName('add')[0];
+const deletebutton = document.getElementsByClassName('delete')[0];
 
+deletebutton.addEventListener('click', () => {
+    window.location.href = '../delete.html';
+});
 addbutton.addEventListener('click', () => {
     window.location.href = '../add.html';
 });
