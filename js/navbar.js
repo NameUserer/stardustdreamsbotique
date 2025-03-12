0a6da4bf84a13b68062d17abc0626259e5ed26ea
Iyvena <szalay.viktoria.1049@dszcbaross.edu.hu>
2025-03-12 09:18:12 +0100

    navbar login check

--- js/navbar.js
+++ js/navbar.js
@@ -3,18 +3,29 @@ const mail = document.getElementsByClassName('mail')[0];
 const wl= document.getElementsByClassName('wl')[0];
 const cart= document.getElementsByClassName('cart')[0];
 
+function isLoggedIn() {
+    return document.cookie.includes("loggedIn=true");
+}
+
+// Function to handle restricted navigation
+function restrictedNav(page) {
+    if (isLoggedIn()) {
+        window.location.href = `../${page}.html`;
+    } else {
+        Swal.fire({
+            icon: 'warning',
+            title: 'Access Denied',
+            text: 'You need to log in for this function to be available!',
+        });
+    }
+}
+
+// Event Listeners
+mail.addEventListener('click', () => restrictedNav('mail'));
+wl.addEventListener('click', () => restrictedNav('wishlist'));
+cart.addEventListener('click', () => restrictedNav('cart'));
+
+// Home is always accessible
 home.addEventListener('click', () => {
     window.location.href = '../home.html';
-});
-
-mail.addEventListener('click', () => {
-    window.location.href = '../mail.html';
-});
-
-wl.addEventListener('click', () => {
-    window.location.href = '../wishlist.html';
-});
-
-cart.addEventListener('click', () => {
-    window.location.href = '../cart.html';
 });
