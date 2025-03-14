14bccd097a425860e09e1ba0b14682ce0c26fc99
Iyvena <szalay.viktoria.1049@dszcbaross.edu.hu>
2025-03-14 10:52:05 +0100

    filter3

--- js/home.js
+++ js/home.js
@@ -63,7 +63,7 @@ async function applyFilters() {
   const selectedTypes = Array.from(document.querySelectorAll(".filter-section:nth-child(2) .filter:checked"))
       .map(checkbox => checkbox.value);
   
-  let url = "/filter?";
+  let url = "api/filterProducts";
   if (selectedCategories.length) url += `category_id=${selectedCategories.join(',')}&`;
   if (selectedTypes.length) url += `type_id=${selectedTypes.join(',')}`;
   
