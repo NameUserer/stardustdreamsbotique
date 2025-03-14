b96c6c94c64a599a90b5c41a86d1bff3398c3669
kpisti18 <k.istvanzoltan@gmail.com>
2025-03-14 14:35:44 +0100

    isLoggedIn bugfix

--- js/navbar.js
+++ js/navbar.js
@@ -6,6 +6,7 @@ const cart= document.getElementsByClassName('cart')[0];
 async function isLoggedIn() {
   const response = await fetch('/api/auth/isLoggedIn', { credentials: 'include' });
   const data = await response.json();
+  console.log(data)
   return data.loggedIn; // true vagy false
 }
 
