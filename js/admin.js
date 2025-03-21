76b9929e04860c8adc4be9753200ccd10d7dc3cb
Iyvena <szalay.viktoria.1049@dszcbaross.edu.hu>
2025-03-21 10:17:34 +0100

    admin delete2

--- js/admin.js
+++ js/admin.js
@@ -150,7 +150,7 @@ async function deleteProduct(productId) {
     }
   
     try {
-      const response = await fetch(`/api/products/deleteProduct/${productId}`, {
+      const response = await fetch(`/api/products/${productId}`, {
         method: "DELETE",
         credentials: "include"
       });
